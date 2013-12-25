7ce21a7dd3d3c84b1c5a5e71ee60dd4c0213566b
Yang <zhyang@xiaomi.com>
2013-12-25 15:09:00 +0800

    add Counter methods

--- test.js
+++ test.js
@@ -3,11 +3,10 @@ var _ = require("underscore");
 require("./underscore.collection");
 
 describe('Counter', function(){
-    var data = [1,2,3,4,1,3,3]
-    
+    var data = [1,2,3,4,1,3,3];
+    var count = new _.Counter(data);
 
-    describe('#init()', function(){
-        var count = new _.Counter(data);
+    describe('#init', function(){
         it("should count the array", function(){
             assert.equal(count['1'], 2);
             assert.equal(count['2'], 1);
@@ -15,7 +14,37 @@ describe('Counter', function(){
             assert.equal(count['4'], 1);
         });
 
-    });    
+    }); 
+
+    describe('#element()', function(){
+        it("should return the same arrary except ordering", function(){
+            // value type problem
+            assert.equal(count.elements().length, data.length);
+        });
+    });   
+
+    // describe('#most_common(n)', function(){
+    //     it("should the most common data", function(){
+
+    //     })
+
+    // })
+
+    describe('#subtract(anotherCounter)', function(){
+        it("should subtracted from counter.", function(){
+            var anotherCounter = new _.Counter([1,1,1,2,3,5])      
+            count.subtract(anotherCounter);
+            assert.equal(count['1'], -1);
+            assert.equal(count['2'], 0);
+            assert.equal(count['3'], 2);
+            assert.equal(count['4'], 1);
+            assert.equal(count['5'], -1);
+
+        })
+
+    })
+
+
 
 });
 
--- underscore.collection.js
+++ underscore.collection.js
@@ -2,26 +2,86 @@
 !function(root, underscore){
   'use strict';
   var _ = underscore;
+
+  _.fillArray = function(value, len) {
+        var arr = [],
+            i;
+        for (i=0; i < len; i++) {
+            arr.push(value);
+        };
+        return arr;
+    }
+
+
   _.Counter = function(arraryData){
-        var dict = {},
+        var self = {},
             i, // for loop index
             length, // for loop length
-            value; // fetch the array value by index
+            value // fetch the array value by index
+            ;
+
+        self._dict = {};
+
         for(i=0, length=arraryData.length; i < length; i++){
             value = arraryData[i];
-            if(value in dict){
-                dict[value]++ ;
+            if(value in self._dict){
+                self._dict[value]++ ;
             }else{
-                dict[value] = 1;
+                self._dict[value] = 1;
             }
         }
 
-        return dict;
+
+        _.each(self._dict, function(v, k){
+            self[k] = v;
+        });
+
+        // typef is a function which is used to cast value type
+        self.elements = function(typef){
+            var array = [];
+            _.each(self._dict, function(v, k){
+                array = array.concat(_.fillArray(k, v));
+            })
+            if(typef){
+                return _.map(array, typef);
+            }
+            return array;
+        }
+
+        self.most_common = function(n, typef){
+            // user orderdictionary 
+        }
+
+        self.subtract = function(anotherCounter){
+            _.each(anotherCounter._dict, function(v, k){
+                if(k in self._dict){
+                    self[k] -= v;
+                }else{
+                    self[k] = -v;
+                }
+
+            })
+        }
+
+        self.update = function(arraryData){
+
+        }
+
+        self._get_counter = function(key){
+            return self._dict[key] || 0;
+        }
+
+
+        return self;
   }// 
+
+
+
+
 
   _.OrderDictionary = function(dict, key){
 
-        
+
 
 
 
